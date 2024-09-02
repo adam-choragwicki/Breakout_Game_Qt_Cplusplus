67ad0e2c2a85d56572fea3d07a296be596f8e899
Adam Choragwicki <adam.choragwicki.dev@gmail.com>
2024-09-02 20:51:23 +0200

    Stage 46

--- src/backend/config_prod.cpp
+++ src/backend/config_prod.cpp
@@ -1,9 +1,9 @@
 #include "config_prod.h"
 
 const int ConfigProd::Arena::ROWS_COUNT = 10;
-const int ConfigProd::Arena::COLUMNS_COUNT = 25;
+const int ConfigProd::Arena::COLUMNS_COUNT = 30;
 
 const int ConfigProd::Brick::WIDTH = 50;
-const int ConfigProd::Brick::HEIGHT = 20;
+const int ConfigProd::Brick::HEIGHT = 25;
 
 const bool ConfigProd::GPU_OPTIMIZATION = true;
